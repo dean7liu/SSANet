4a94238928cb14caf186eb4561d5ff0b8b51154c
dean7liu <141602873+dean7liu@users.noreply.github.com>
2025-02-01 22:14:11 +0800

    Create __init__.py

---
+++ model/__init__.py
@@ -0,0 +1,8 @@
+from .SSANet import SSANet
+from .SSANet_dist import SSANet_dist
+from .loss import model_loss_train, model_loss_test
+
+__models__ = {
+    "SSANet": SSANet,
+    "SSANet_dist": SSANet_dist
+}
