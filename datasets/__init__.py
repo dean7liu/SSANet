4f0f5669df2756cc5423d0099456a9fc2d0baa5b
dean7liu <141602873+dean7liu@users.noreply.github.com>
2025-02-01 22:34:33 +0800

    Add files via upload

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,1 +1,9 @@
+from .kitti_dataset_1215 import KITTIDataset
+from .sceneflow_dataset import SceneFlowDatset
 
+
+__datasets__ = {
+    "sceneflow": SceneFlowDatset,
+    "kitti": KITTIDataset,
+
+}
